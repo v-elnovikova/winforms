c3850f2195db531667b1effefe067eac10a4bb6e
Devendar Reddy A <dreddy@microsoft.com>
2022-08-02 12:09:33 -0700

    ddraft

---
+++ src/System.Windows.Forms.Primitives/src/System/LocalAppContextSwitches/RuntimeTargetFramework.TargetFramework.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Windows.Forms.Primitives.RuntimeTargetFramework
+{
+    internal static partial class RuntimeTargetFramework
+    {
+        internal class TargetFramework
+        {
+            public string? Name { get; set; }
+            public string? Version { get; set; }
+        }
+    }
+}
---
+++ src/System.Windows.Forms.Primitives/src/System/LocalAppContextSwitches/RuntimeTargetFramework.cs
@@ -0,0 +1,84 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace System.Windows.Forms.Primitives.RuntimeTargetFramework
+{
+    internal static partial class RuntimeTargetFramework
+    {
+        private static bool readConfig;
+        private static TargetFramework? s_framework;
+        public static TargetFramework? Framework
+        {
+            get
+            {
+                if (!readConfig)
+                {
+                    ReadTargetFrameworkFromConfig();
+                }
+
+                return s_framework;
+            }
+        }
+
+        public static void ReadTargetFrameworkFromConfig()
+        {
+            string runtimeConfigPath = $"{Process.GetCurrentProcess().ProcessName}.runtimeconfig.json";
+
+            var jsonDocumentOptions = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true,
+                CommentHandling = JsonCommentHandling.Skip
+            };
+
+            using (var stream = File.OpenRead(runtimeConfigPath))
+            using (JsonDocument doc = JsonDocument.Parse(stream, jsonDocumentOptions))
+            {
+                JsonElement root = doc.RootElement;
+                if (root.TryGetProperty("runtimeOptions", out var runtimeOptionsRoot))
+                {
+                    if (runtimeOptionsRoot.TryGetProperty("framework", out var framework))
+                    {
+                        var runtimeConfigFramework = new TargetFramework();
+                        string? name = null;
+                        string? version = null;
+                        foreach (var property in framework.EnumerateObject())
+                        {
+                            if (property.Name.Equals(nameof(name), StringComparison.OrdinalIgnoreCase))
+                            {
+                                name = property.Value.GetString();
+                            }
+
+                            if (property.Name.Equals(nameof(version), StringComparison.OrdinalIgnoreCase))
+                            {
+                                version = property.Value.GetString();
+                            }
+                        }
+
+                        if (name == null || version == null)
+                        {
+                            s_framework = null;
+                        }
+                        else
+                        {
+                            s_framework = new TargetFramework
+                            {
+                                Name = name,
+                                Version = version
+                            };
+                        }
+                    }
+                    else
+                    {
+                        s_framework = null;
+                    }
+                }
+
+                readConfig = true;
+            }
+        }
+    }
+}
