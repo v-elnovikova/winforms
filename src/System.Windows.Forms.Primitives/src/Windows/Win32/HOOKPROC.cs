3f539c207272be3f95385d4f40d5b8fdad8c6f62
Jeremy Kuhne <jkuhne@microsoft.com>
2022-08-08 10:31:16 -0700

    Introduce IHandle<T> (#7546)
    * Add global usings for cswin32
    
    Adding global usings for cswin32 to facilitate porting and help understand the state of conflicts. `using foo =` statements are places where we currently have conficts with identically named types.
    
    * Introduce IHandle<T>
    
    Add typed IHandle to keep handles typed. Add HandleRef<T> that derives from IHandle<T> to use where direct binding with IHandle<T> is not always necessary.
    
    Port a number of User32 APIs to cswin32 to vet design.

--- src/System.Windows.Forms.Primitives/src/Windows/Win32/HOOKPROC.cs
+++ src/System.Windows.Forms.Primitives/src/Windows/Win32/HOOKPROC.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -7,3 +7,4 @@ using Windows.Win32.Foundation;
 namespace Windows.Win32;
 
 internal delegate LRESULT HOOKPROC(int nCode, WPARAM wParam, LPARAM lParam);
+
